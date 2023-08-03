cd6c764d75a8deed42c328035d406e8a263588cb
Sundar Nambuvel <sunprema@gmail.com>
2023-08-03 15:46:24 -0500

    login changes, using context

--- src/app/retailPassports/page.tsx
+++ src/app/retailPassports/page.tsx
@@ -1,6 +1,26 @@
-const RetailPassports = () => {
+
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { notFound, redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+
+import { Database } from '@/types/supabase';
+
+const RetailPassports = async () => {
+    const supabase = createServerComponentClient<Database>({
+        cookies,
+    })
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+
     return (
-        <h1> Hello</h1>
+        <>    
+        <h1> Hello : </h1>
+        <pre>
+            {JSON.stringify(session, null , 2)}
+        </pre>
+        </>       
+
     )
 
 }
